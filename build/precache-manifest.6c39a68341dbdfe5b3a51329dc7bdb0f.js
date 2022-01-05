dc31e06b4fb72dfafed87637584ee5162d8be9c1
YUGG <yogendrajhala650@gmail.com>
2022-01-05 10:01:31 +0530

    First Deploy

---
+++ build/precache-manifest.6c39a68341dbdfe5b3a51329dc7bdb0f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cc34a74fa7f59e0fcc886425593fc51",
+    "url": "/Notes/index.html"
+  },
+  {
+    "revision": "e1b017533e6e40d3693f",
+    "url": "/Notes/static/js/2.8a02f188.chunk.js"
+  },
+  {
+    "revision": "0d8e396fac0d265da592",
+    "url": "/Notes/static/js/main.d517d650.chunk.js"
+  },
+  {
+    "revision": "177054f91f78be324b15",
+    "url": "/Notes/static/js/runtime-main.ca9b7305.js"
+  }
+]);
